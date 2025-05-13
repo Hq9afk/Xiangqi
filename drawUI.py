7cf3e80c4cb33d69b3bf3321378a78f250ac5747
Huy Copper <blashblap@gmail.com>
2025-05-13 19:25:42 +0700

    fix UI

--- drawUI.py
+++ drawUI.py
@@ -1,100 +1,73 @@
 import pygame as p
 import setting as s
 import loading as l
-
 import chessEngine
+import button as b
+import playWithMachine as pWM
 import time
-
+from main import st
 chessManImg = l.loadChessMan()
 boardImg = l.loadBoard()
 lightImg = l.loadLight()
 squareImg = l.loadSquare()
 
 
-"""
+'''
 This function draw the valid move of the selected chessman
-"""
-
-
-def drawValid(screen, gs):
+'''
+def drawValid(screen,gs):
     listValid = gs.checkValid(gs.selectedCell)
     start = s.GRID
     for i in listValid:
-        screen.blit(
-            lightImg,
-            p.Rect(
-                start[1] + i[1] * start[2],
-                start[0] + i[0] * start[2],
-                s.CELL_SIZE,
-                s.CELL_SIZE,
-            ),
-        )
-
+        screen.blit(lightImg, p.Rect(start[1]+ i[1]*start[2],start[0]+i[0]*start[2], s.CELL_SIZE, s.CELL_SIZE))
 
 check = True
 
-"""
+'''
 This function draw all the game state
-"""
+'''
+def drawGameState(screen,gs: chessEngine.State,st):
+    screen.blit(boardImg,(0,0))     # Vẽ bàn cờ
 
+    drawChessMan(screen,gs.board)     # Vẽ các quân cờ
 
-def drawGameState(screen, gs: chessEngine.State, st):
-    screen.blit(boardImg, (0, 0))
+    # Vẽ các biểu tượng khác (như "win", "lose", v.v.)
     global check
     if gs.checkEnd()[0]:
-        drawEndGame(screen, gs)
+        drawEndGame(screen,gs)
 
+    # Vẽ biểu tượng "checkmate"    
     elif gs.checkMate() and check:
         startTime = p.time.get_ticks()
-        drawChessMate(screen, gs)
+        drawChessMate(screen,gs)
         if p.time.get_ticks() - startTime >= 2000:
             check = False
 
+    # Vẽ biểu tượng "thinking"
     elif (gs.after and gs.redMove) or (not gs.after and not gs.redMove):
         drawAIThink(screen) if st else None
-    drawChessMan(screen, gs.board)
-
+    
+    # Vẽ các nước đi hợp lệ
     if gs.selectedCell != ():
-        drawValid(screen, gs)
-        screen.blit(
-            squareImg,
-            p.Rect(
-                s.GRID[1] + gs.selectedCell[1] * s.GRID[2],
-                s.GRID[0] + gs.selectedCell[0] * s.GRID[2],
-                s.CELL_SIZE,
-                s.CELL_SIZE,
-            ),
-        )
-    drawFoot(screen, gs)
+        drawValid(screen,gs)
+        screen.blit(squareImg, p.Rect(s.GRID[1]+ gs.selectedCell[1]*s.GRID[2],s.GRID[0]+gs.selectedCell[0]*s.GRID[2], s.CELL_SIZE, s.CELL_SIZE))
+   
+    drawFoot(screen,gs)     # Vẽ nước đi cuối cùng
 
-
-"""
+'''
 This function draw the chessman on the board
-"""
-
-
-def drawChessMan(screen, board):
+'''
+def drawChessMan(screen,board):
     start = s.GRID
-    for i in range(s.DIMENSION + 1):
+    for i in range(s.DIMENSION+1):
         for j in range(s.DIMENSION):
             chessMan = board[i][j]
-            if chessMan != "---":
-                screen.blit(
-                    chessManImg[chessMan],
-                    p.Rect(
-                        start[1] + j * start[2],
-                        start[0] + i * start[2],
-                        s.CELL_SIZE,
-                        s.CELL_SIZE,
-                    ),
-                )
+            if chessMan != '---':  
+                screen.blit(chessManImg[chessMan],p.Rect(start[1]+j*start[2],start[0]+i*start[2],s.CELL_SIZE,s.CELL_SIZE))
 
-
-"""
+'''
 This function draw the last move 
-"""
-
-
+'''
 def drawFoot(screen, gs: chessEngine.State):
     if gs.moveLog == []:
         return
@@ -102,159 +75,87 @@ def drawFoot(screen, gs: chessEngine.State):
     startCol = gs.moveLog[-1].startCol
     endRow = gs.moveLog[-1].endRow
     endCol = gs.moveLog[-1].endCol
-    screen.blit(
-        squareImg,
-        p.Rect(
-            s.GRID[1] + startCol * s.GRID[2],
-            s.GRID[0] + startRow * s.GRID[2],
-            s.CELL_SIZE,
-            s.CELL_SIZE,
-        ),
-    )
-    screen.blit(
-        squareImg,
-        p.Rect(
-            s.GRID[1] + endCol * s.GRID[2],
-            s.GRID[0] + endRow * s.GRID[2],
-            s.CELL_SIZE,
-            s.CELL_SIZE,
-        ),
-    )
-
-
-"""
+    screen.blit(squareImg, p.Rect(s.GRID[1]+ startCol*s.GRID[2],s.GRID[0]+startRow*s.GRID[2], s.CELL_SIZE, s.CELL_SIZE))
+    screen.blit(squareImg, p.Rect(s.GRID[1]+ endCol*s.GRID[2],s.GRID[0]+endRow*s.GRID[2], s.CELL_SIZE, s.CELL_SIZE))
+    
+'''
 This function is draw when the King is checked
-"""
-
-
+'''
 def drawChessMate(screen, gs: chessEngine.State):
     checkimg = l.loadCheckMate()
-    screen.blit(
-        checkimg,
-        (
-            s.WIDTH / 2 - checkimg.get_width() / 2,
-            s.SCREEN_HEIGHT / 2 - checkimg.get_height() / 2,
-        ),
-    )
+    screen.blit(checkimg,(s.WIDTH/2 - checkimg.get_width()/2, s.START_Y + 5))
 
-
-"""
+'''
 This function draw the end game
-"""
-
-
+'''
 def drawEndGame(screen, gs: chessEngine.State):
     print("end game")
     if gs.checkEnd()[0]:
-        winner = "RED" if gs.checkEnd()[1] == "r" else "BLACK"
+        winner= 'RED' if gs.checkEnd()[1] =='r' else 'BLACK'
         p.font.init()
-        print(winner, " WIN")
-        myFont = p.font.SysFont("Comic Sans MS", 30)
+        print(winner," WIN")
+        myFont = p.font.SysFont('Comic Sans MS', 30)
         textSurface = myFont.render(winner + " WIN", False, (0, 0, 0))
-        screen.blit(
-            textSurface,
-            (
-                s.WIDTH / 2 - textSurface.get_width() / 2,
-                s.SCREEN_HEIGHT / 2 - textSurface.get_height() / 2,
-            ),
-        )
-
-
-"""
+        screen.blit(textSurface,(s.WIDTH/2 - textSurface.get_width()/2, s.START_Y + 5))
+        
+'''
 This is the function draw the UI when AI thinking
-"""
-
-
+'''
 def drawAIThink(screen):
     p.font.init()
-    myFont = p.font.SysFont("Comic Sans MS", 30)
-    textSurface = myFont.render("ChaCa's thinking...", False, (0, 0, 0))
-    screen.blit(
-        textSurface,
-        (
-            s.WIDTH / 2 - textSurface.get_width() / 2,
-            s.SCREEN_HEIGHT / 2 - textSurface.get_height() / 2,
-        ),
-    )
+    myFont = p.font.SysFont('Comic Sans MS', 30)
+    textSurface = myFont.render("thinking...", False, (167, 133, 96))
+    screen.blit(textSurface,(s.WIDTH/2 - textSurface.get_width()/2, s.START_Y + 5))
 
 
-"""
-This function draw the title of the game"""
-
-
+'''
+This function draw the title of the game'''
 def drawTitle(screen, x, y, width, height, text):
     p.font.init()
-    tFont = p.font.SysFont("Comic Sans MS", 30)
-
+    tFont = p.font.SysFont('Comic Sans MS', 30)
+    
     Title = tFont.render(text, True, (55, 255, 255))
     Rect = Title.get_rect()
-    Rect.center = ((x + width / 2), (y + height / 2))
+    Rect.center = ((x + width/2), (y + height/2))
     screen.blit(Title, Rect)
     return Title
 
 
-"""
+'''
 This is the function draw the initial button
-"""
-
-
+'''
 def drawButton(screen, x, y, width, height, text):
-    Button = p.Rect(x, y, width, height)
+    button_surface = p.Surface((width, height), p.SRCALPHA)
+    
     p.font.init()
-    bFont = p.font.SysFont("Comic Sans MS", 30)
-    content = bFont.render(text, True, (55, 55, 55))
+    font_path = "impact.ttf"  
+    tFont = p.font.Font(font_path, 30) 
+    content = tFont.render(text, True, (230, 200, 100))  # Màu chữ trắng
     Rect = content.get_rect()
-    Rect.center = Button.center
-    p.draw.rect(screen, (255, 255, 255), Button)
-    screen.blit(content, Rect)
-    return Button
-
-
-"""
+    Rect.center = (width // 2, height // 2)
+    button_surface.blit(content, Rect)
+    screen.blit(button_surface, (x, y))
+    return p.Rect(x, y, width, height)
+    
+    
+'''
 This is the function draw the start screen
-"""
-
-
+'''
 def drawStart(screen, gs):
-    drawTitle(screen, s.TITLE_WIDTH_X, s.TITLE_HEIGHT_Y, 0, 0, "Chinese Chess")
-    randomBut = drawButton(
-        screen, s.BUTTEXT_X, s.BUTTEXT_Y, s.BUT_TEXT, s.BUT_TEXT / 6, "Play with Random"
-    )
-    chaca = drawButton(
-        screen,
-        s.BUTTEXT_X,
-        s.BUTTEXT_Y + s.BUT_TEXT / 3,
-        s.BUT_TEXT,
-        s.BUT_TEXT / 6,
-        "Play with ChaCa",
-    )
-
-    test = drawButton(
-        screen,
-        s.BUTTEXT_X,
-        s.BUTTEXT_Y + 2 * s.BUT_TEXT / 3,
-        s.BUT_TEXT,
-        s.BUT_TEXT / 6,
-        "Play with ChaCaPro",
-    )
-    solo = drawButton(
-        screen,
-        s.BUTTEXT_X,
-        s.BUTTEXT_Y + s.BUT_TEXT,
-        s.BUT_TEXT,
-        s.BUT_TEXT / 6,
-        "Watch them play",
-    )
+    background = l.loadMenuBackground()
+    screen.blit(background, (0, 0))
+    
+    randomBut = drawButton(screen, s.BUTTEXT_X , s.BUTTEXT_Y, s.BUT_TEXT, s.BUT_TEXT/6, "Play with Random")
+    
+    test = drawButton(screen, s.BUTTEXT_X , s.BUTTEXT_Y + 110, s.BUT_TEXT, s.BUT_TEXT/6, "Play with AI")
+    solo = drawButton(screen, s.BUTTEXT_X , s.BUTTEXT_Y + 220, s.BUT_TEXT, s.BUT_TEXT/6, "Test mode")
 
     click = p.mouse.get_pressed()[0]
-    if click == 1:
+    if click==1:
         mouse = p.mouse.get_pos()
         if randomBut.collidepoint(mouse):
             time.sleep(0.2)
             return 1
-        elif chaca.collidepoint(mouse):
-            time.sleep(0.2)
-            return 2
         elif test.collidepoint(mouse):
             time.sleep(0.2)
             return 3
