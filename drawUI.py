ee24eacd95c9f191382def96e9732f27ad873787
Hq9afk <hieubuiduc1@gmail.com>
2025-05-13 22:14:20 +0700

    commit

--- drawUI.py
+++ drawUI.py
@@ -2,8 +2,6 @@ import pygame as p
 import setting as s
 import loading as l
 import chessEngine
-import button as b
-import playWithMachine as pWM
 import time
 
 chessManImg = l.loadChessPiece()
@@ -11,15 +9,24 @@ boardImg = l.loadBoard()
 lightImg = l.loadLight()
 squareImg = l.loadSquare()
 
-
-'''
+"""
 This function draw the valid move of the selected chessman
-'''
-def drawValid(screen,gs):
+"""
+
+def drawValid(screen, gs):
     listValid = gs.checkValid(gs.selectedCell)
     start = s.GRID
     for i in listValid:
-        screen.blit(lightImg, p.Rect(start[1]+ i[1]*start[2],start[0]+i[0]*start[2], s.CELL_SIZE, s.CELL_SIZE))
+        screen.blit(
+            lightImg,
+            p.Rect(
+                start[1] + i[1] * start[2],
+                start[0] + i[0] * start[2],
+                s.CELL_SIZE,
+                s.CELL_SIZE,
+            ),
+        )
+
 
 check = True
 
@@ -91,8 +98,8 @@ def drawEndGame(screen, gameState: chessEngine.State):
     if gameState.checkEnd()[0]:
         winner= 'RED' if gameState.checkEnd()[1] =='r' else 'BLACK'
         p.font.init()
-        print(winner," WIN")
-        myFont = p.font.SysFont('Comic Sans MS', 30)
+        print(winner, " WIN")
+        myFont = p.font.SysFont("Comic Sans MS", 30)
         textSurface = myFont.render(winner + " WIN", False, (0, 0, 0))
         screen.blit(textSurface,(s.WIDTH/2 - textSurface.get_width()/2, s.SCREEN_HEIGHT/2 - textSurface.get_height()/2))
         
@@ -102,8 +109,8 @@ This is the function draw the UI when AI is thinking
 def drawAIThink(screen):
     p.font.init()
     myFont = p.font.SysFont('Comic Sans MS', 30)
-    textSurface = myFont.render("ChaCa is thinking...", False, (0, 0, 0))
-    screen.blit(textSurface,(s.WIDTH/2 - textSurface.get_width()/2, s.SCREEN_HEIGHT/2 - textSurface.get_height()/2))
+    textSurface = myFont.render("thinking...", False, (167, 133, 96))
+    screen.blit(textSurface,(s.WIDTH/2 - textSurface.get_width()/2, s.START_Y + 5))
 
 
 def drawTitle(screen, x, y, width, height, text):
@@ -117,44 +124,47 @@ def drawTitle(screen, x, y, width, height, text):
     return Title
 
 
-'''
+"""
 This is the function draw the initial button
-'''
+"""
+
+
 def drawButton(screen, x, y, width, height, text):
     button_surface = p.Surface((width, height), p.SRCALPHA)
     
     p.font.init()
     font_path = "impact.ttf"  
     tFont = p.font.Font(font_path, 30) 
-    content = tFont.render(text, True, (230, 200, 100))  # Màu chữ trắng
+    content = tFont.render(text, True, (230, 200, 100))
     Rect = content.get_rect()
-    Rect.center = Button.center
-    p.draw.rect(screen, (255, 255, 255), Button)
-    screen.blit(content, Rect)
-    return Button
+    Rect.center = (width // 2, height // 2)
+    button_surface.blit(content, Rect)
+    screen.blit(button_surface, (x, y))
+    return p.Rect(x, y, width, height)
 
 
 '''
 This is the function draw the start screen
 '''
 def drawGameMenuScreen(screen, gs):
-    drawTitle(screen, s.TITLE_WIDTH_X, s.TITLE_HEIGHT_Y, 0, 0, "Chinese Chess")
-    randomModeButton = drawButton(screen, s.BUTTEXT_X , s.BUTTEXT_Y, s.BUT_TEXT, s.BUT_TEXT/6, "Play with Random")
-    ChaCaModeButton = drawButton(screen, s.BUTTEXT_X,  s.BUTTEXT_Y + s.BUT_TEXT/3, s.BUT_TEXT, s.BUT_TEXT/6, "Play with ChaCa")
+    background = l.loadMenuBackground()
+    screen.blit(background, (0, 0))
     
-    ChaCaProModeButton = drawButton(screen, s.BUTTEXT_X , s.BUTTEXT_Y + 2*s.BUT_TEXT/3, s.BUT_TEXT, s.BUT_TEXT/6, "Play with ChaCaPro")
-    AI_VS_RANDOM_ModeButton = drawButton(screen, s.BUTTEXT_X , s.BUTTEXT_Y + s.BUT_TEXT, s.BUT_TEXT, s.BUT_TEXT/6, "Watch them play")
+    randomBut = drawButton(screen, s.BUTTEXT_X , s.BUTTEXT_Y, s.BUT_TEXT, s.BUT_TEXT/6, "Play with Random")
     
-    isMouseClicked = p.mouse.get_pressed()[0]
-    if isMouseClicked:
-        clickPosition = p.mouse.get_pos()
-        if randomModeButton.collidepoint(clickPosition):
+    test = drawButton(screen, s.BUTTEXT_X , s.BUTTEXT_Y + 110, s.BUT_TEXT, s.BUT_TEXT/6, "Play with AI")
+    solo = drawButton(screen, s.BUTTEXT_X , s.BUTTEXT_Y + 220, s.BUT_TEXT, s.BUT_TEXT/6, "Test mode")
+
+    click = p.mouse.get_pressed()[0]
+    if click==1:
+        mouse = p.mouse.get_pos()
+        if randomBut.collidepoint(mouse):
             time.sleep(0.2)
             return 1
-        elif ChaCaProModeButton.collidepoint(clickPosition):
-            time.sleep(0.2)
-            return 2
-        elif AI_VS_RANDOM_ModeButton.collidepoint(clickPosition):
+        elif test.collidepoint(mouse):
             time.sleep(0.2)
             return 3
+        elif solo.collidepoint(mouse):
+            time.sleep(0.2)
+            return 4
     return -1
